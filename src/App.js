ca8df9d5aa96e374bcea8a7be3697d5fc27b3f6e
EdithOrt <eoc.diseno@gmail.com>
2020-11-23 16:00:46 -0600

    login with email and password activated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '@material-ui/core'
+import Login from './components/Login'
 
 function App() {
     return (
@@ -7,6 +8,7 @@ function App() {
           <p>Inicializando proyecto diamond generating</p>
           <Button variant="contained" color="primary">Hey botón de material UI</Button>
           <p>También ya está instalado react-router-dom y material ui icons</p>
+          <Login />
       </div>
     );
   }
