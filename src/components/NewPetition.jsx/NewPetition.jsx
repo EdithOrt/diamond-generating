cabf3ea97d1286b507eefb6c7fd5fc6a7aa0a1d7
EdithOrt <eoc.diseno@gmail.com>
2020-11-25 19:14:46 -0600

    section new requests design

--- src/components/NewPetition.jsx/NewPetition.jsx
+++ src/components/NewPetition.jsx/NewPetition.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Menu from '../layout/Menu';
 import AppBarHeader from '../layout/AppBarHeader';
+import { Box, Typography, Divider } from '@material-ui/core';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import image from '../../Assets/images/new-request.png'
+import { makeStyles } from '@material-ui/core/styles';
 
 function NewPetition() {
     return (
@@ -8,7 +12,18 @@ function NewPetition() {
             <Menu />
             <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
                 <AppBarHeader title='Crear solicitudes' />
-                <p>Aqui va nueva solicitudes</p>
+                <Box style={{ display: 'flex', alignItems: 'center', height:'550px', justifyContent:'space-evenly'}}>
+                    <Box>
+                        <img src={image}/>
+                    </Box>
+                        <Divider orientation="vertical" style={{backgroundColor:'#DADADA', height:'450px'}} />
+                    <Box style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                        <AddCircleOutlineIcon style={{color:'#1D376C', fontSize:'80px'}} />
+                        <Typography style={{color:'#1D376C', fontSize:'24px', marginTop:'20px'}}>
+                            Crear solicitud
+                        </Typography>
+                    </Box>
+                </Box>
             </div>
         </div>
     );
